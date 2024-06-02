ce6ece908dbadb50d12d428f2e637d9a4fdafd54
Shishir Halder <s.halder0108@gmail.com>
2024-06-02 20:58:49 +0600

    first commit: initialize next app with shadcn-ui

---
+++ src/app/layout.jsx
@@ -0,0 +1,39 @@
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Explore TipTap",
+  description: "A rich text editor",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
